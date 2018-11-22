e57bb74afbd4185199e1f959f923f9cae2223856
wanghai <wanghai@teddymobile.cn>
2018-11-22 18:16:49 +0800

    集成牛逼的文本阅读器

--- app/src/main/java/com/example/newbiechen/ireader/ui/activity/BookDetailActivity.java
+++ app/src/main/java/com/example/newbiechen/ireader/ui/activity/BookDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.newbiechen.ireader.ui.activity;
 
+
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +10,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
