886507316aa96655e8a4d8db2fffd70d59aaae6f
wanghai <wanghai@teddymobile.cn>
2018-11-22 12:00:18 +0800

    init

---
+++ mylibrary/src/main/java/holanovel/ted/com/mylibrary/base/adapter/BaseViewHolder.java
@@ -0,0 +1,18 @@
+package holanovel.ted.com.mylibrary.base.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by newbiechen on 17-5-17.
+ */
+
+public class BaseViewHolder<T> extends RecyclerView.ViewHolder{
+    public IViewHolder<T> holder;
+
+    public BaseViewHolder(View itemView, IViewHolder<T> holder) {
+        super(itemView);
+        this.holder = holder;
+        holder.initView();
+    }
+}
